17e1e181280379b97fcb97a7414070376f1334fc
Atsushi Shimura <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2018-04-30 11:00:33 +0900

    PLA用Feed作成対応　

--- make4Feed.py
+++ make4Feed.py
@@ -4,7 +4,7 @@ import sys
 wb = openpyxl.load_workbook(sys.argv[1])
 sheet = wb[wb.sheetnames[0]]
 
-rowCnt = 1
+rowCnt = 2
 text_dict = {}
 discription_dict = {}
 
@@ -12,12 +12,21 @@ while True :
   pid = sheet["A" + str(rowCnt)].value
   if pid is None :
     break
-  rowCnt = rowCnt + 1
   pid = pid.replace('c', '')
-  text_dict[pid] = sheet["E" + str(rowCnt)].value
+  text_dict[pid] = sheet["C" + str(rowCnt)].value
+  discription_dict[pid] = sheet["E" + str(rowCnt)].value
+  rowCnt = rowCnt + 1
 
+textFile = open('text.txt', 'w')
 for key, value in text_dict.items():
-  print (key)
-  print (value)
+  textFile.write("'{0}' => '{1}'\n".format(key, value))
+
+textFile.close()
+
+discriptionFile = open('discription.txt', 'w')
+for key, value in discription_dict.items():
+  discriptionFile.write("'{0}' => '{1}'\n".format(key, value))
+
+discriptionFile.close()
 
 wb.close()
